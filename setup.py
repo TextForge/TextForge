24c3d30a2c48d2a2cddd52aef3307de216b188e4
shehanzsaleem <56260108+shehanzsaleem@users.noreply.github.com>
2023-04-25 03:14:55 +0530

    Create Setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="textforge-metafeatures",
+    version="0.0.1",
+    description="TextForge Metadata Extraction Library",
+    packages=["textforge-metafeatures"],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "textstat",
+        "scipy",
+        "nltk",
+        "scikit-learn",
+        "textblob"
+    ]
+)
