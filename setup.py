af9d797a2acaed457bda8545c200f03bb59bf65c
shehanzsaleem <56260108+shehanzsaleem@users.noreply.github.com>
2023-04-25 03:48:06 +0530

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="TextForge",
-    version="0.0.2",
+    version="0.0.3",
     description="TextForge Metadata Extraction Library",
     packages=["TextForge"],
     install_requires=[
